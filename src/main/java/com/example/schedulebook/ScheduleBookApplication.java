51461042c2810bea13cbbac99009b64f04f55391
bjw446 <bjw446@naver.com>
2026-06-18 13:53:00 +0900

    Initail Java setup

---
+++ src/main/java/com/example/schedulebook/ScheduleBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schedulebook/ScheduleBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
