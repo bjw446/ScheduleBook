786985250e946d757c3dbb44d8bb3a31c4be728c
bjw446 <bjw446@naver.com>
2026-06-22 21:51:34 +0900

    refactor : 웹소켓 엔드포인트 기본 값 추가

--- src/main/java/com/example/schedulebook/common/config/WebSocketConfig.java
+++ src/main/java/com/example/schedulebook/common/config/WebSocketConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    @Value("${app.websocket.allowed-origins}")
+    @Value("${app.websocket.allowed-origins:*}")
     private String[] allowedOrigins;
 
     @Override
