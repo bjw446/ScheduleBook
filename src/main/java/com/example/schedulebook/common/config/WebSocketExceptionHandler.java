412c4ade065425a8417d7fc36349892978484c7f
bjw446 <bjw446@naver.com>
2026-06-23 23:11:31 +0900

    feat : 웹소켓 익셉션 핸들러 로그 추가

--- src/main/java/com/example/schedulebook/common/config/WebSocketExceptionHandler.java
+++ src/main/java/com/example/schedulebook/common/config/WebSocketExceptionHandler.java
@@ -2,15 +2,19 @@ package com.example.schedulebook.common.config;
 
 import com.example.schedulebook.common.exception.BaseException;
 import com.example.schedulebook.common.response.StompErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
+@Slf4j
 public class WebSocketExceptionHandler {
     @MessageExceptionHandler(BaseException.class)
     @SendToUser("/queue/errors")
     public StompErrorResponse handleBaseException(BaseException e) {
+        log.warn("WebSocket 메시지 처리 중 실패, code = {}, message = {}", e.getErrorEnum().name(), e.getMessage());
+
         return new StompErrorResponse(
                 e.getErrorEnum().name(),
                 e.getErrorEnum().getStatus(),
