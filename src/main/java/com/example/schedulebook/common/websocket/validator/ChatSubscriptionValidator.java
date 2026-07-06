a45115d86e204c64e85da02e91aa2f34d0577568
bjw446 <bjw446@naver.com>
2026-07-06 19:14:02 +0900

    feat : 웹소컷 권한 검증 추가

---
+++ src/main/java/com/example/schedulebook/common/websocket/validator/ChatSubscriptionValidator.java
@@ -0,0 +1,37 @@
+package com.example.schedulebook.common.websocket.validator;
+
+import com.example.schedulebook.common.enums.ErrorEnum;
+import com.example.schedulebook.common.exception.BaseException;
+import com.example.schedulebook.domain.chat.repository.ChatRoomMemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Component
+@RequiredArgsConstructor
+public class ChatSubscriptionValidator implements SubscriptionValidator{
+    private static final Pattern CHAT_PATTERN = Pattern.compile("^/topic/chat/(\\d+).*");
+    private final ChatRoomMemberRepository chatRoomMemberRepository;
+
+    @Override
+    public boolean supports(String destination) {
+        return destination.startsWith("/topic/chat/");
+    }
+
+    @Override
+    public void validate(Long userId, String destination) {
+        Matcher matcher = CHAT_PATTERN.matcher(destination);
+
+        if (!matcher.matches()) {
+            throw new BaseException(ErrorEnum.INVALID_INPUT);
+        }
+
+        Long roomId = Long.parseLong(matcher.group(1));
+
+        chatRoomMemberRepository.findActiveByChatRoomIdAndUserId(roomId, userId).orElseThrow(
+                () -> new BaseException(ErrorEnum.CHAT_ROOM_FORBIDDEN)
+        );
+    }
+}
---
+++ src/main/java/com/example/schedulebook/common/websocket/validator/ScheduleSubscriptionValidator.java
@@ -0,0 +1,44 @@
+package com.example.schedulebook.common.websocket.validator;
+
+import com.example.schedulebook.common.enums.ErrorEnum;
+import com.example.schedulebook.common.exception.BaseException;
+import com.example.schedulebook.domain.schedule.repository.ScheduleRepository;
+import com.example.schedulebook.domain.scheduleshare.enums.ScheduleShareStatus;
+import com.example.schedulebook.domain.scheduleshare.repository.ScheduleShareRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Component
+@RequiredArgsConstructor
+public class ScheduleSubscriptionValidator implements SubscriptionValidator{
+    private static final Pattern PATTERN = Pattern.compile("^/topic/schedule/(\\d+)(/.*)?$");
+    private final ScheduleRepository scheduleRepository;
+    private final ScheduleShareRepository scheduleShareRepository;
+
+    @Override
+    public boolean supports(String destination) {
+        return destination.startsWith("/topic/schedule/");
+    }
+
+    @Override
+    public void validate(Long userId, String destination) {
+        Matcher matcher = PATTERN.matcher(destination);
+
+        if (!matcher.matches()) {
+            throw new BaseException(ErrorEnum.INVALID_INPUT);
+        }
+
+        Long scheduleId = Long.parseLong(matcher.group(1));
+
+        if (scheduleRepository.existsByIdAndUser_Id(scheduleId, userId)) {
+            return;
+        }
+
+        scheduleShareRepository.findActiveRelation(scheduleId, userId, ScheduleShareStatus.ACTIVE).orElseThrow(
+                () -> new BaseException(ErrorEnum.SCHEDULE_FORBIDDEN)
+        );
+    }
+}
