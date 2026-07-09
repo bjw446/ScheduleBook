92aa57c2a80af08e0519dfa5d393f427718d578e
bjw446 <bjw446@naver.com>
2026-07-10 02:26:02 +0900

    refactor : Test 파일 수정

--- src/test/java/com/example/schedulebook/ScheduleBookApplicationTests.java
+++ src/test/java/com/example/schedulebook/ScheduleBookApplicationTests.java
@@ -1,15 +1,12 @@
 package com.example.schedulebook;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
 @ActiveProfiles("test")
 class ScheduleBookApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
