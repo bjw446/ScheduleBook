02f270cd9dddf310bcb464dbe40461d87d7a264d
bjw446 <bjw446@naver.com>
2026-07-25 02:12:59 +0900

    feat : Outbox 이벤트 타입 클래스 지정 및 Publisher 추가

--- src/main/java/com/example/schedulebook/domain/outbox/publisher/OutboxPublisher.java
+++ src/main/java/com/example/schedulebook/domain/outbox/publisher/OutboxPublisher.java
@@ -1,10 +1,6 @@
 package com.example.schedulebook.domain.outbox.publisher;
 
-import com.example.schedulebook.common.enums.ErrorEnum;
-import com.example.schedulebook.common.exception.BaseException;
-import com.example.schedulebook.domain.comment.event.CommentCreatedEvent;
 import com.example.schedulebook.domain.outbox.entity.Outbox;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
@@ -16,20 +12,14 @@ public class OutboxPublisher {
     private final ObjectMapper objectMapper;
     private final ApplicationEventPublisher applicationEventPublisher;
 
-    public void publish(Outbox outbox) throws JsonProcessingException {
-        switch (outbox.getEventType()) {
-            case COMMENT_CREATED -> {
-                CommentCreatedEvent event = objectMapper.readValue(
-                        outbox.getPayload(),
-                        CommentCreatedEvent.class
-                );
+    public void publish(Outbox outbox) throws Exception {
+        Class<?> eventClass = outbox.getEventType().getEventClass();
 
-                applicationEventPublisher.publishEvent(event);
-            }
+        Object event = objectMapper.readValue(
+                outbox.getPayload(),
+                eventClass
+        );
 
-            // TODO 이벤트 들 추가 예정
-
-            default -> throw new BaseException(ErrorEnum.INVALID_OUTBOX_EVENT_TYPE);
-        }
+        applicationEventPublisher.publishEvent(event);
     }
 }
